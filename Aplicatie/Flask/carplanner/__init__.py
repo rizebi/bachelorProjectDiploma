e849fbaab3c802aec7cc2df29b866f0b1f062f5b
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2019-05-16 22:28:56 +0300

    Starting creating of Models

--- Aplicatie/Flask/carplanner/__init__.py
+++ Aplicatie/Flask/carplanner/__init__.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
@@ -21,16 +20,11 @@ app.config['SECRET_KEY'] = 'mysecret'
 ### DATABASE SETUPS ############
 ###############################
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-'''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mypassword@mysql/car-planner'
 
 db = SQLAlchemy(app)
-Migrate(app,db)
 
-
+'''
 ###########################
 #### LOGIN CONFIGS #######
 #########################
--- Aplicatie/Flask/carplanner/models.py
+++ Aplicatie/Flask/carplanner/models.py
@@ -1,6 +1,6 @@
-from car-planner import db,login_manager
+from car-planner import db, login_manager
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 # By inheriting the UserMixin we get access to a lot of built-in attributes
 # which we will be able to call in our views!
@@ -9,57 +9,87 @@ from flask_login import UserMixin
 # is_anonymous()
 # get_id()
 
-'''
+
 # The user_loader decorator allows flask-login to load the current user
 # and grab their id.
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+  return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
 
-    # Create a table in the db
-    __tablename__ = 'users'
+  # Create a table in the db
+  __tablename__ = 'useri'
 
-    id = db.Column(db.Integer, primary_key = True)
-    profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
-    # This connects BlogPosts to a User Author.
-    posts = db.relationship('BlogPost', backref='author', lazy=True)
+  IDUser = db.Column(db.Integer, primary_key = True)
+  imagineProfil = db.Column(db.String(20), nullable=False, default='default_profile.png')
+  numeUser = db.Column(db.String(30))
+  prenumeUser = db.Column(db.String(30))
+  email = db.Column(db.String(30))
+  parola = db.Column(db.String(128))
+  numeCompanie = db.Column(db.String(30))
 
-    def __init__(self, email, username, password):
-        self.email = email
-        self.username = username
-        self.password_hash = generate_password_hash(password)
+  masini = db.relationship('masini', backref='proprietar', lazy='dynamic')
 
-    def check_password(self,password):
-        # https://stackoverflow.com/questions/23432478/flask-generate-password-hash-not-constant-output
-        return check_password_hash(self.password_hash,password)
+  def __init__(self, numeUser, prenumeUser, email, parola, numeCompanie):
+    self.numeUser = numeUser
+    self.prenumeUser = prenumeUser
+    self.email = email
+    self.numeCompanie = numeCompanie
+    self.parola = generate_password_hash(parola)
 
-    def __repr__(self):
-        return f"UserName: {self.username}"
+  def check_password(self, parola):
+    # https://stackoverflow.com/questions/23432478/flask-generate-password-hash-not-constant-output
+    return check_password_hash(self.parola, parola)
+
+  def __repr__(self):
+    return f"Nume: {self.numeUser}, Prenume: {self.prenumeUser}"
+
+
+class User(db.Model, UserMixin):
+
+  # Create a table in the db
+  __tablename__ = 'users'
+  # lazy = 'dynamic'
+
+  id = db.Column(db.Integer, primary_key = True)
+  profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
+  email = db.Column(db.String(64), unique=True, index=True)
+  username = db.Column(db.String(64), unique=True, index=True)
+  password_hash = db.Column(db.String(128))
+  # This connects BlogPosts to a User Author.
+  posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+  def __init__(self, email, username, password):
+    self.email = email
+    self.username = username
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self,password):
+    # https://stackoverflow.com/questions/23432478/flask-generate-password-hash-not-constant-output
+    return check_password_hash(self.password_hash,password)
+
+  def __repr__(self):
+    return f"UserName: {self.username}"
 
 class BlogPost(db.Model):
-    # Setup the relationship to the User table
-    users = db.relationship(User)
+  # Setup the relationship to the User table
+  users = db.relationship(User)
 
-    # Model for the Blog Posts on Website
-    id = db.Column(db.Integer, primary_key=True)
-    # Notice how we connect the BlogPost to a particular author
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    title = db.Column(db.String(140), nullable=False)
-    text = db.Column(db.Text, nullable=False)
+  # Model for the Blog Posts on Website
+  id = db.Column(db.Integer, primary_key=True)
+  # Notice how we connect the BlogPost to a particular author
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  title = db.Column(db.String(140), nullable=False)
+  text = db.Column(db.Text, nullable=False)
 
-    def __init__(self, title, text, user_id):
-        self.title = title
-        self.text = text
-        self.user_id =user_id
+  def __init__(self, title, text, user_id):
+    self.title = title
+    self.text = text
+    self.user_id =user_id
 
 
-    def __repr__(self):
-        return f"Post Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
-'''
+  def __repr__(self):
+    return f"Post Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
