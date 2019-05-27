7851a922b8d74051dd9f5aeceac16adca785aa06
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2019-05-27 23:54:06 +0300

    Tarife + Contact + Despre pages done

--- Aplicatie/Flask/carplanner/core/views.py
+++ Aplicatie/Flask/carplanner/core/views.py
@@ -6,18 +6,16 @@ core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
-    '''
-    This is the home page view. Notice how it uses pagination to show a limited
-    number of posts by limiting its query size and then calling paginate.
-    '''
-    #page = request.args.get('page', 1, type=int)
-    #blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
-    return render_template('index.html')#,blog_posts=blog_posts)
+    return render_template('index.html')
 
-#@core.route('/info')
-#def info():
-    '''
-    Example view of any other "core" page. Such as a info page, about page,
-    contact page. Any page that doesn't really sync with one of the models.
-    '''
-#    return render_template('info.html')
+@core.route('/despre')
+def despre():
+    return render_template('despre.html')
+
+@core.route('/tarife')
+def tarife():
+    return render_template('tarife.html')
+
+@core.route('/contact')
+def contact():
+    return render_template('contact.html')
