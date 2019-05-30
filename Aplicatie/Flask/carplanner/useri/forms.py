a46a09e4a890997e9df0f7abd23168c485d46744
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2019-05-30 22:20:26 +0300

    Starting of register / log in

--- Aplicatie/Flask/carplanner/useri/forms.py
+++ Aplicatie/Flask/carplanner/useri/forms.py
@@ -1,12 +1,47 @@
+# Form Based Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
 
-'''
-class BlogPostForm(FlaskForm):
-    # no empty titles or text possible
-    # we'll grab the date automatically from the Model later
-    title = StringField('Title', validators=[DataRequired()])
-    text = TextAreaField('Text', validators=[DataRequired()])
-    submit = SubmitField('BlogPost')
-'''
+# User Based Imports
+from flask_login import current_user
+from carplanner.models import User
+
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    parola = PasswordField('Parola', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    numeUser = StringField('Nume', validators=[DataRequired()])
+    prenumeUser = StringField('Prenume', validators=[DataRequired()])
+    numeCompanie = StringField('Nume Companie')
+    parola = PasswordField('Parola', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
+    parolaConfirm = PasswordField('Confirmare Parola', validators=[DataRequired()])
+    submit = SubmitField('Inregistrare!')
+
+    def validate_email(self, field):
+        # Check if not None for that user email!
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError('Your email has been registered already!')
+
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    numeUser = StringField('Nume', validators=[DataRequired()])
+    prenumeUser = StringField('Prenume', validators=[DataRequired()])
+    numeCompanie = StringField('Nume Companie')
+    picture = FileField('Incarca fotografie de profil', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Actualizeaza')
+
+    def validate_email(self, field):
+        # Check if not None for that user email!
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has been registered already!')
--- Aplicatie/Flask/carplanner/useri/views.py
+++ Aplicatie/Flask/carplanner/useri/views.py
@@ -1,74 +1,108 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
-from flask_login import current_user, login_required
-
+from flask_login import login_user, current_user, logout_user, login_required
 from carplanner import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from carplanner.models import User
-from carplanner.useri.forms import UserForm
+from carplanner.useri.forms import RegistrationForm, LoginForm, UpdateUserForm
+from carplanner.useri.picture_handler import add_profile_pic
 
-useri = Blueprint('useri',__name__)
-'''
-@useri.route('/create',methods=['GET','POST'])
 
-@login_required
-def create_post():
-    form = BlogPostForm()
+useri = Blueprint('useri', __name__)
+
+
+@useri.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
 
     if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    numeUser=form.numeUser.data,
+                    prenumeUser=form.prenumeUser.data,
+                    numeCompanie=form.numeCompanie.data,
+                    parola=form.parola.data)
 
-        blog_post = BlogPost(title=form.title.data,
-                             text=form.text.data,
-                             user_id=current_user.id
-                             )
-        db.session.add(blog_post)
+        db.session.add(user)
         db.session.commit()
-        flash("Blog Post Created")
-        return redirect(url_for('core.index'))
+        flash('Multumim pentru inregistrare! Te poti loga acum.')
+        return redirect(url_for('useri.login'))
+    return render_template('register.html', form=form)
 
-    return render_template('create_post.html',form=form)
+@useri.route('/login', methods=['GET', 'POST'])
+def login():
 
-
-# int: makes sure that the blog_post_id gets passed as in integer
-# instead of a string so we can look it up later.
-@blog_posts.route('/<int:blog_post_id>')
-def blog_post(blog_post_id):
-    # grab the requested blog post by id number or return 404
-    blog_post = BlogPost.query.get_or_404(blog_post_id)
-    return render_template('blog_post.html',title=blog_post.title,
-                            date=blog_post.date,post=blog_post
-    )
-
-@blog_posts.route("/<int:blog_post_id>/update", methods=['GET', 'POST'])
-@login_required
-def update(blog_post_id):
-    blog_post = BlogPost.query.get_or_404(blog_post_id)
-    if blog_post.author != current_user:
-        # Forbidden, No Access
-        abort(403)
-
-    form = BlogPostForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        blog_post.title = form.title.data
-        blog_post.text = form.text.data
-        db.session.commit()
-        flash('Post Updated')
-        return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
-    # Pass back the old blog post information so they can start again with
-    # the old text and title.
-    elif request.method == 'GET':
-        form.title.data = blog_post.title
-        form.text.data = blog_post.text
-    return render_template('create_post.html', title='Update',
-                           form=form)
+        # Grab the user from our User Models table
+        user = User.query.filter_by(email = form.email.data).first()
+
+        # Check that the user was supplied and the password is right
+        # The verify_password method comes from the User object
+        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
+
+        if user.check_password(form.parola.data) and user is not None:
+            #Log in the user
+
+            login_user(user)
+            flash('Autentificare reusita!')
+
+            # If a user was trying to visit a page that requires a login
+            # flask saves that URL as 'next'.
+            next = request.args.get('next')
+
+            # So let's now check if that next exists, otherwise we'll go to
+            # the welcome page.
+            if next == None or not next[0]=='/':
+                next = url_for('core.index')
+
+            return redirect(next)
+    return render_template('login.html', form=form)
 
 
-@blog_posts.route("/<int:blog_post_id>/delete", methods=['POST'])
-@login_required
-def delete_post(blog_post_id):
-    blog_post = BlogPost.query.get_or_404(blog_post_id)
-    if blog_post.author != current_user:
-        abort(403)
-    db.session.delete(blog_post)
-    db.session.commit()
-    flash('Post has been deleted')
+
+
+@useri.route("/logout")
+def logout():
+    logout_user()
     return redirect(url_for('core.index'))
+
+
+
+@useri.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+        print(form)
+        if form.picture.data:
+            email = current_user.email
+            pic = add_profile_pic(form.picture.data, email)
+            current_user.profile_image = pic
+
+        current_user.email = form.email.data
+        current_user.numeUser = form.numeUser.data
+        current_user.prenumeUser = form.prenumeUser.data
+        current_user.numeCompanie = form.numeCompanie.data
+
+        db.session.commit()
+        flash('Datele contului au fost actualizate cu succes.')
+        return redirect(url_for('useri.account'))
+
+    elif request.method == 'GET':
+        form.email.data = current_user.email
+        form.numeUser.data = current_user.numeUser
+        form.prenumeUser.data = current_user.prenumeUser
+        form.numeCompanie.data = current_user.numeCompanie
+
+    profile_image = url_for('static', filename='profile_pics/' + current_user.profile_image)
+    return render_template('account.html', profile_image=profile_image, form=form)
+
+'''
+@useri.route("/<username>")
+def user_cars(email):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
 '''
