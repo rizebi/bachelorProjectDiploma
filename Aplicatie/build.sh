3f9157df3ce59e70eb7aca2c28089275748562d4
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2020-01-12 17:47:47 +0200

    Add Prometheus

--- Aplicatie/build.sh
+++ Aplicatie/build.sh
@@ -30,16 +30,21 @@ if [ "$1" == "push" ]; then
   sudo docker image rm localhost:444/emailer-image
   sudo docker image rm localhost:444/mysql:5.7
   sudo docker image rm localhost:444/alpine
+  sudo docker image rm localhost:444/prometheus
+
 
   sudo docker tag flask-image localhost:444/flask-image
   sudo docker tag emailer-image localhost:444/emailer-image
   sudo docker tag $(docker images | grep mysql | tr -s " " | cut -d" " -f3) localhost:444/mysql:5.7
   sudo docker tag alpine localhost:444/alpine
+  sudo docker tag $(docker images | grep prometheus | tr -s " " | cut -d" " -f3) localhost:444/prometheus
+
 
   sudo docker push localhost:444/flask-image
   sudo docker push localhost:444/emailer-image
   sudo docker push localhost:444/mysql:5.7
   sudo docker push localhost:444/alpine
+  sudo docker push localhost:444/prometheus
 
   exit
 fi
@@ -50,16 +55,20 @@ if [ "$1" == "pull" ]; then
   sudo docker image rm emailer-image
   sudo docker image rm $(docker images | grep mysql | tr -s " " | cut -d" " -f3)
   sudo docker image rm alpine
+  sudo docker image rm $(docker images | grep prometheus | tr -s " " | cut -d" " -f3)
+
 
   sudo docker pull localhost:444/flask-image
   sudo docker pull localhost:444/emailer-image
   sudo docker pull localhost:444/mysql:5.7
   sudo docker pull localhost:444/alpine
+  sudo docker pull localhost:444/prometheus
 
   sudo docker tag localhost:444/flask-image flask-image
   sudo docker tag localhost:444/emailer-image emailer-image
   sudo docker tag localhost:444/mysql:5.7 mysql:5.7
   sudo docker tag localhost:444/alpine alpine
+  sudo docker tag localhost:444/alpine prom/prometheus
 
   exit
 fi
@@ -110,10 +119,12 @@ if [ "$1" == "first" ]; then
   sudo mkdir /var/lib/car-planner/mysql
   sudo mkdir /var/lib/car-planner/mysql-conf
   sudo mkdir /var/lib/car-planner/flask
+  sudo mkdir /var/lib/car-planner/prometheus
 
   # copy application files
   sudo cp -R ./Flask/* /var/lib/car-planner/flask
   sudo cp -R ./MySQL/* /var/lib/car-planner/mysql-conf
+  sudo cp -R ./Prometheus/* /var/lib/car-planner/prometheus
 
   sleep 1
 fi
@@ -123,11 +134,16 @@ if [ "$1" != "stop" ] && [ "$1" != "first" ]; then
   sudo rm -rf /var/lib/car-planner/flask/*
   echo "Removing /var/lib/car-planner/mysql-conf/*"
   sudo rm -rf /var/lib/car-planner/mysql-conf/*
+  echo "Removing /var/lib/car-planner/prometheus/*"
+  sudo rm -rf /var/lib/car-planner/prometheus/*
+
 
   echo "Copying to /var/lib/car-planner/flask/*"
   sudo cp -R ./Flask/* /var/lib/car-planner/flask/
   echo "Copying to /var/lib/car-planner/mysql-conf/*"
   sudo cp -R ./MySQL/* /var/lib/car-planner/mysql-conf/
+  echo "Copying to /var/lib/car-planner/prometheus/*"
+  sudo cp -R ./Prometheus/* /var/lib/car-planner/prometheus/
 fi
 
 if [ "$1" == "flask" ] || [ "$1" == "first" ]; then
